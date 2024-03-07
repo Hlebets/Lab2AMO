ff44ce0b1add0b1171e4b89706961dda1745e8a9
Hlebéts <glib.kobryn@gmail.com>
2024-03-08 00:15:47 +0200

    Final Version

--- Lab2AMO.py
+++ Lab2AMO.py
@@ -1,11 +1,12 @@
 import math
+import numpy as np
 import matplotlib.pyplot as plt
 
 def f(x, a):
-    return math.exp(a * x) * (1 + x * x) * math.sin(x) / (x + 2)
+    return np.exp(a * x) * (1 + x * x) * np.sin(x) / (x + 2)
 
 
-def simpson(a, b, n, ai):
+def simpson_integral(a, b, n, ai):
     h = 0.001
     summ = f(a, a) + f(b, a)
     for i in range(1, n):
@@ -16,11 +17,23 @@ def simpson(a, b, n, ai):
 
 
 a = [0.019, 0.127, 0.346, 0.417, 0.527, 0.696]
-print("a\t     integral")
+wavalues = [0.2581540067158702, 0.2779425229697118, 0.3234648726721269, 0.3399509191810630, 0.3673539787612951, 0.4143086261231443]
+print("a     | integral     | wavalues")
 for i in range(6):
     ai = a[i]
-    result = simpson(0, 1, 1000, ai)
-    print(ai, "\t", result)
+    waivalues = wavalues[i]
+    result = simpson_integral(0, 1, 1000, ai)
+    print("{:<4} | {:<12} | {:<10}".format(ai, round(result, 9), round(waivalues, 9)))
 
-plt.plot(a, [simpson(0, 1, 1000, ai) for ai in a])
+x = np.linspace(0, 1, int((1 - 0) / 0.001) + 1)
+plt.figure(figsize=(10, 7))
+plt.title("Інтеграли функції при кожному а")
+
+for ai in a:
+    y = f(x, ai)
+    integral = simpson_integral(0, 1, 1000, ai)
+    plt.plot(x, y, label=f"а = {ai:.3f}, Int = {integral:.9f}")
+    plt.fill_between(x, y, color='pink', alpha=0.4)
+
+plt.legend()
 plt.show()
